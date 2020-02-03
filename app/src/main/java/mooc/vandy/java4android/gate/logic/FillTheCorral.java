3fbdd7bee28987bc449525685619071eaba7a38b
Joe Motacek <cleanshooter@gmail.com>
2020-02-03 20:07:46 +0200

    Update FillTheCorral.java

--- app/src/main/java/mooc/vandy/java4android/gate/logic/FillTheCorral.java
+++ app/src/main/java/mooc/vandy/java4android/gate/logic/FillTheCorral.java
@@ -14,6 +14,7 @@ public class FillTheCorral {
      * Reference to the OutputInterface.
      */
     private OutputInterface mOut;
+    private static final int STARTING_PASTURE = 5;
 
     /**
      * Constructor initializes the field.
@@ -24,15 +25,55 @@ public class FillTheCorral {
 
     // TODO -- Fill your code in here
     public void setCorralGates(Gate[] gate, Random selectDirection) {
-
+        for (int i = 0; i < gate.length; i++){
+            gate[i].setSwing(selectDirection.nextInt(3)-1);
+        }
     }
 
     public boolean anyCorralAvailable(Gate[] corral) {
-        return true;
+        boolean isOneOpen = false;
+        for (int i = 0; i < corral.length; i++){
+            if(corral[i].getSwingDirection() == corral[i].IN){
+                isOneOpen = true;
+            }
+        }
+        return isOneOpen;
     }
 
     public int corralSnails(Gate[] corral, Random rand) {
-        return 0;
+        int pasture = STARTING_PASTURE;
+        int counter = 0;
+
+        mOut.println("Initial gate setup:");
+        for (int i = 0; i < corral.length; i++){
+            if(corral[i].getSwingDirection() == corral[i].IN){
+                mOut.println("Gate "+i+": This gate is open and swings to enter the pen only");
+            }
+            if(corral[i].getSwingDirection() == corral[i].OUT){
+                mOut.println("Gate "+i+": This gate is open and swings to exit the pen only");
+            }
+            if(corral[i].getSwingDirection() == corral[i].CLOSED){
+                mOut.println("Gate "+i+": This gate is closed");
+            }
+        }
+
+        // Simulation Loop
+        do {
+            int snailsToMove = rand.nextInt(pasture)+1;
+            int selectedGate = rand.nextInt(corral.length);
+            mOut.println(snailsToMove + " are trying to move through corral " + selectedGate);
+            if (corral[selectedGate].getSwingDirection() == Gate.IN) {
+                pasture = pasture - snailsToMove;
+            } else if (corral[selectedGate].getSwingDirection() == Gate.OUT) {
+                pasture = pasture + snailsToMove;
+            }
+            // If the gate is close there is no change.
+            counter++;
+        } while (pasture > 0 && counter < 20);
+
+        // Return the number of time we had to run the loop
+        mOut.println("It took "+counter+" attempts to corral all of the snails.");
+        return counter;
     }
 
 }
